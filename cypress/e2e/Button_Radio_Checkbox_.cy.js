db819d1818c22e51ce6b78b0d4101e46ca52566b
chidl96 <chidl6996@gmail.com>
2022-06-03 11:40:14 +0700

    add more testcase button/radio/checkbox

--- cypress/e2e/Button_Radio_Checkbox_.cy.js
+++ cypress/e2e/Button_Radio_Checkbox_.cy.js
@@ -9,21 +9,21 @@ import "cypress-iframe";
 import "cypress-xpath";
 
 describe("Topic 09 Exercise", () => {
-  it("TC_01_Button", () => {
-    cy.visit("https://www.fahasa.com/customer/account/create");
-    cy.handleIframe(
-      "iframe#preview-notification-frame.st_preview_frame_modal",
-      "#NC_IMAGE1"
-    );
-    cy.get(".popup-login-tab-login").click;
-    cy.get("li.popup-login-tab-login").click();
-    cy.get("#login_username").type("chidl@unstatic.co");
-    cy.get("#login_password").type("12345678");
-    const btnLogin = "button.fhs-btn-login";
-    cy.get(btnLogin)
-      // .should('be.enabled')
-      .should("have.css", "background-color", "rgb(201, 33, 39)");
-  });
+  // it("TC_01_Button", () => {
+  //   cy.visit("https://www.fahasa.com/customer/account/create");
+  //   cy.handleIframe(
+  //     "iframe#preview-notification-frame.st_preview_frame_modal",
+  //     "#NC_IMAGE1"
+  //   );
+  //   cy.get(".popup-login-tab-login").click;
+  //   cy.get("li.popup-login-tab-login").click();
+  //   cy.get("#login_username").type("chidl@unstatic.co");
+  //   cy.get("#login_password").type("12345678");
+  //   const btnLogin = "button.fhs-btn-login";
+  //   cy.get(btnLogin)
+  //     // .should('be.enabled')
+  //     .should("have.css", "background-color", "rgb(201, 33, 39)");
+  // });
 
   it("TC_02_Default_Checkbox_Radio_Button", () => {
     cy.visit("https://demos.telerik.com/kendo-ui/checkbox/index");
@@ -45,4 +45,17 @@ describe("Topic 09 Exercise", () => {
     
   });
 
+  it('TC_03_Custom_Radio_Checkbox_p1', () => {
+    cy.visit("https://material.angular.io/components/checkbox/examples");
+    cy.xpath("//span[text()='Checked']//preceding::input").click({force:true});
+    
+  });
+
+  it('TC_04_Custom_Radio_Checkbox_p2', () => {
+    cy.visit("https://docs.google.com/forms/d/e/1FAIpQLSfiypnd69zhuDkjKgqvpID9kwO29UCzeCVrGGtbNPZXQok0jA/viewform");
+    cy.xpath("//div[@aria-label='Mì Quảng']")
+      .click()
+      .should('have.attr','aria-checked','true')
+
+  });
 });
