10ac4dc5c095993844c42caea7471adb3de735b6
chidl96 <chidl6996@gmail.com>
2022-06-06 13:35:09 +0700

    handle alert

--- cypress/e2e/Button_Radio_Checkbox_.cy.js
+++ cypress/e2e/Button_Radio_Checkbox_.cy.js
@@ -25,37 +25,54 @@ describe("Topic 09 Exercise", () => {
   //     .should("have.css", "background-color", "rgb(201, 33, 39)");
   // });
 
-  it("TC_02_Default_Checkbox_Radio_Button", () => {
-    cy.visit("https://demos.telerik.com/kendo-ui/checkbox/index");
-    cy.xpath(
-      "//label[text()='Dual-zone air conditioning']/preceding-sibling::input"
-    )
-      .click()
-      .should("be.checked");
-      cy.xpath(
-        "//label[text()='Dual-zone air conditioning']/preceding-sibling::input"
-      )
-        .click()
-        .should("not.be.checked");
-    
-    cy.visit("https://demos.telerik.com/kendo-ui/radiobutton/index");
-    if (cy.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input").should('not.be.checked')) {
-        cy.xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input").click();
-    }
-    
+  // it("TC_02_Default_Checkbox_Radio_Button", () => {
+  //   cy.visit("https://demos.telerik.com/kendo-ui/checkbox/index");
+  //   cy.xpath(
+  //     "//label[text()='Dual-zone air conditioning']/preceding-sibling::input"
+  //   )
+  //     .click()
+  //     .should("be.checked");
+  //   cy.xpath(
+  //     "//label[text()='Dual-zone air conditioning']/preceding-sibling::input"
+  //   )
+  //     .click()
+  //     .should("not.be.checked");
+
+  //   cy.visit("https://demos.telerik.com/kendo-ui/radiobutton/index");
+  //   if (
+  //     cy
+  //       .xpath("//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input")
+  //       .should("not.be.checked")
+  //   ) {
+  //     cy.xpath(
+  //       "//label[text()='2.0 Petrol, 147kW']/preceding-sibling::input"
+  //     ).click();
+  //   }
+  // });
+
+  // it("TC_03_Custom_Radio_Checkbox_p1", () => {
+  //   cy.visit("https://material.angular.io/components/checkbox/examples");
+  //   cy.xpath("//span[text()='Checked']//preceding::input").click({
+  //     force: true,
+  //   });
+  // });
+
+  // it("TC_04_Custom_Radio_Checkbox_p2", () => {
+  //   cy.visit(
+  //     "https://docs.google.com/forms/d/e/1FAIpQLSfiypnd69zhuDkjKgqvpID9kwO29UCzeCVrGGtbNPZXQok0jA/viewform"
+  //   );
+  //   cy.xpath("//div[@aria-label='Mì Quảng']")
+  //     .click()
+  //     .should("have.attr", "aria-checked", "true");
+  // });
+
+  it('TC_05', () => {
+    cy.visit("https://register.rediff.com/register/register.php");
+    cy.xpath("//input[@id='Register']").click();
+    cy.on('window:alert',(t)=>{
+      //assertions
+      expect(t).to.contains('Your full name');
+   })
   });
 
-  it('TC_03_Custom_Radio_Checkbox_p1', () => {
-    cy.visit("https://material.angular.io/components/checkbox/examples");
-    cy.xpath("//span[text()='Checked']//preceding::input").click({force:true});
-    
-  });
-
-  it('TC_04_Custom_Radio_Checkbox_p2', () => {
-    cy.visit("https://docs.google.com/forms/d/e/1FAIpQLSfiypnd69zhuDkjKgqvpID9kwO29UCzeCVrGGtbNPZXQok0jA/viewform");
-    cy.xpath("//div[@aria-label='Mì Quảng']")
-      .click()
-      .should('have.attr','aria-checked','true')
-
-  });
 });
